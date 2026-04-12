e26f6140ee0eed9074fbbf930a4414a3c736cd21
Richard <RichardBekhazi@gmail.com>
2026-04-12 19:12:29 -0400

    Disable auth guard for Apple pages

--- apple/js/guard.js
+++ apple/js/guard.js
@@ -1,13 +1,1 @@
-import { auth } from "./firebase-init.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-const PROTECTED = ["money.html", "pictures.html"];
-const path = location.pathname.toLowerCase();
-const needsLogin = PROTECTED.some(p => path.endsWith(p));
-
-onAuthStateChanged(auth, user => {
-  if (needsLogin && !user) {
-    const next = encodeURIComponent(location.pathname + location.search);
-    location.replace(`login.html?next=${next}`);
-  }
-});
+// No authentication required for Apple Farewell pages
